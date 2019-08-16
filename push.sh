f4e4deb36fa2ed9019cced663e813c801a187f17
zrj <1518130788@qq.com>
2019-08-16 15:28:39 +0800

    add push.sh

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /root/test/learngit && cp /mnt/hgfs/update/debian9.txt debian9.txt
-git add /root/test/learngit/*
+git add /root/test/learngit/debian9.txt
 git commit -m "`date`"
 git push origin master
 
